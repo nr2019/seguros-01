bee220c7098113b822dfc5512bc0971a9fc034f3
Nahuel <nahuel.ryzy@gmail.com>
2019-06-08 13:09:57 -0300

    CORS

--- server/routes/cotizacion.js
+++ server/routes/cotizacion.js
@@ -7,6 +7,12 @@ const Cotizacion = require('../models/cotizacion');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-whith, Content-Type, Accept");
+    next();
+});
+
 /*****************************************************************/
 // GET
 /*****************************************************************/
--- server/routes/direccion.js
+++ server/routes/direccion.js
@@ -7,6 +7,12 @@ const Direccion = require('../models/direccion');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-whith, Content-Type, Accept");
+    next();
+});
+
 /*****************************************************************/
 // GET
 /*****************************************************************/
